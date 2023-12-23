872af0315b5a873190316ad872d2b232a16b3d5d
fancop <113712008+fancop@users.noreply.github.com>
2023-12-23 14:43:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ ANTS_PER_ANTHILL_MAX = 10
 ANTS_PER_ANTHILL_MIN = 1
 
 class GameObject:
-    """
+    '''
     Базовый класс для всех игровых объектов.
-    """
+    '''
 
     def __init__(self, y, x, image):
-        """
+        '''
         Инициализация объекта с указанными координатами и изображением.
-        """
+        '''
         self.y = y
         self.x = x
         self.image = image
 
     def move(self, direction, field):
-        """
+        '''
         Перемещает объект в указанном направлении на игровом поле.
-        """
+        '''
         new_y, new_x = self.y, self.x
 
         if direction == 'up' and self.y > 0 and not isinstance(field.cells[self.y - 1][self.x].content, Anthill):
             new_y -= 1
-        elif direction == "down" and self.y < field.rows - 1 and not isinstance(
+        elif direction == 'down' and self.y < field.rows - 1 and not isinstance(
             field.cells[self.y + 1][self.x].content, Anthill
         ):
             new_y += 1
-        elif direction == "left" and self.x > 0 and not isinstance(field.cells[self.y][self.x - 1].content, Anthill):
+        elif direction == 'left' and self.x > 0 and not isinstance(field.cells[self.y][self.x - 1].content, Anthill):
             new_x -= 1
-        elif direction == "right" and self.x < field.cols - 1 and not isinstance(
+        elif direction == 'right' and self.x < field.cols - 1 and not isinstance(
             field.cells[self.y][self.x + 1].content, Anthill
         ):
             new_x += 1
@@ -50,9 +50,9 @@ class GameObject:
         field.cells[self.y][self.x].content = self
 
     def place_object(self, field):
-        """
+        '''
         Размещает объект на игровом поле.
-        """
+        '''
         if field.cells[self.y][self.x].content is None:
             field.cells[self.y][self.x].content = self
         else:
@@ -67,105 +67,105 @@ class GameObject:
                 field.cells[new_y][new_x].content = self
                 self.y, self.x = new_y, new_x
             else:
-                print(f"Нету клеток для размещения {self.image}!")
+                print(f'Нету клеток для размещения {self.image}!')
 
     def draw(self, field):
-        """
+        '''
         Рисует объект на игровом поле.
-        """
+        '''
         field.cells[self.y][self.x].content = self
 
 
 class Cell:
-    """
+    '''
     Класс, представляющий клетку игрового поля.
-    """
+    '''
 
     def __init__(self, Y=None, X=None):
-        """
+        '''
         Инициализация клетки с пустым изображением и координатами.
-        """
+        '''
         self.image = EMPTY
         self.Y = Y
         self.X = X
         self.content = None
 
     def draw(self):
-        """
+        '''
         Выводит изображение содержимого клетки.
-        """
+        '''
         if self.content:
-            print(self.content.image, end=" ")
+            print(self.content.image, end=' ')
         else:
-            print(self.image, end=" ")
+            print(self.image, end=' ')
 
 
 class Player(GameObject):
-    """
+    '''
     Класс игрока на игровом поле.
-    """
+    '''
 
     def __init__(self, y=None, x=None):
-        """
+        '''
         Инициализация игрока с указанными координатами.
-        """
+        '''
         super().__init__(y, x, PLAYER)
 
     def move(self, direction, field):
-        """
+        '''
         Перемещает игрока в указанном направлении на игровом поле.
-        """
+        '''
         super().move(direction, field)
 
 
 class Ant(GameObject):
-    """
+    '''
     Класс представляющий муравья на игровом поле.
-    """
+    '''
 
     def __init__(self, y, x):
-        """
+        '''
         Инициализация муравья с указанными координатами.
-        """
+        '''
         super().__init__(y, x, ANT)
 
 
 class Anthill(GameObject):
-    """
+    '''
     Класс представляющий муравейник на игровом поле.
-    """
+    '''
 
     def __init__(self, x, y, quantity):
-        """
+        '''
         Инициализация муравейника с указанными координатами и количеством муравьев.
-        """
+        '''
         super().__init__(y, x, ANTHILL)
         self.quantity = quantity
         self.spawn_counter = 0
         self.ants_counter = random.randint(ANTS_PER_ANTHILL_MIN, ANTS_PER_ANTHILL_MAX)
 
     def place(self, field):
-        """
+        '''
         Размещает муравейник на игровом поле.
-        """
+        '''
         super().place_object(field)
 
     def draw(self, field):
-        """
+        '''
         Рисует муравейник на игровом поле.
-        """
+        '''
         super().draw(field)
 
 
 class Field:
-    """
+    '''
     Класс, представляющий игровое поле.
-    """
+    '''
 
     def __init__(self, cell=Cell, player=Player, anthill=Anthill):
-        """
+        '''
         Инициализация игрового поля с указанными параметрами.
-        """
+        '''
         self.game_over = False
         self.rows = ROWS
         self.cols = COLS
@@ -180,25 +180,25 @@ class Field:
         self.player.draw(self)
 
     def draw_rows(self):
-        """
+        '''
         Выводит изображение каждой клетки игрового поля.
-        """
+        '''
         for row in self.cells:
             for cell in row:
                 cell.draw()
             print()
 
     def add_anthill(self, anthill):
-        """
+        '''
         Добавляет муравейник на игровое поле.
-        """
+        '''
         self.anthills.append(anthill)
         anthill.place_object(self)
 
     def get_neighbours(self, y, x):
-        """
+        '''
         Возвращает координаты соседних клеток для заданных координат.
-        """
+        '''
         neighbours_coords = []
         for row in (-1, 0, 1):
             for col in (-1, 0, 1):
@@ -208,9 +208,9 @@ class Field:
         return neighbours_coords
 
     def add_anthills(self):
-        """
+        '''
         Добавляет случайное количество муравейников на игровое поле.
-        """
+        '''
         available_cells = [
             (x, y) for x in range(self.cols) for y in range(self.rows) if (x, y) != (self.player.x, self.player.y)
         ]
@@ -229,9 +229,9 @@ class Field:
         anthill.place_object(self)
 
     def spawn_ants(self):
-        """
+        '''
         Создает новых муравьев в муравейниках.
-        """
+        '''
         for anthill in self.anthills:
             if anthill.ants_counter > 0 and anthill.spawn_counter == 0:
                 anthill_x, anthill_y = anthill.x, anthill.y
@@ -266,9 +266,9 @@ class Field:
         self.move_ants()
 
     def move_ants(self):
-        """
+        '''
         Перемещает муравьев по игровому полю.
-        """
+        '''
         for ant in self.ants:
             neighbours_coords = self.get_neighbours(ant.y, ant.x)
             random.shuffle(neighbours_coords)
@@ -303,21 +303,21 @@ class Field:
             self.game_over = True
 
     def update_statistics(self):
-        """
+        '''
         Обновляет статистику по съеденным и сбежавшим муравьям и выводит ее на экран.
-        """
+        '''
         self.total_ants = self.eaten_ants + self.escaped_ants
-        print("Статистика:")
-        print(f"Все муравьи: {self.total_ants}")
+        print('Статистика:')
+        print(f'Все муравьи: {self.total_ants}')
         print(f"Съеденные мураьи: {self.eaten_ants}")
-        print(f"Сбежавшие муравьи: {self.escaped_ants}")
+        print(f'Сбежавшие муравьи: {self.escaped_ants}')
         input('Если вы посмотрели статистику, то нажмите ENTER, чтобы закончить игру')
 
 
 def clear_screen():
-    """
+    '''
     Очищает экран консоли.
-    """
+    '''
     if os.name == 'nt':
         os.system('cls')
     else:
@@ -325,50 +325,50 @@ def clear_screen():
 
 
 class Game:
-    """
+    '''
     Класс, представляющий игру и управляющий ею.
-    """
+    '''
 
     def __init__(self):
-        """
+        '''
         Инициализация игры.
-        """
+        '''
         self.field = Field()
         self.field.add_anthills()
 
     def handle_keyboard_event(self, event):
-        """
+        '''
         Обрабатывает события клавиатуры и перемещает игрока в соответствии с нажатой клавишей.
-        """
+        '''
         if event.event_type == keyboard.KEY_DOWN:
-            if event.name == "up":
-                self.field.player.move("up", self.field)
-            elif event.name == "down":
-                self.field.player.move("down", self.field)
-            elif event.name == "left":
-                self.field.player.move("left", self.field)
-            elif event.name == "right":
-                self.field.player.move("right", self.field)
+            if event.name == 'up':
+                self.field.player.move('up', self.field)
+            elif event.name == 'down':
+                self.field.player.move('down', self.field)
+            elif event.name == 'left':
+                self.field.player.move('left', self.field)
+            elif event.name == 'right':
+                self.field.player.move('right', self.field)
             elif event.name == 'esc':
-                print("Игра окончена!")
+                print('Игра окончена!')
                 return True
         return False
 
     def update_game_state(self):
-        """
+        '''
         Обновляет состояние игры: рисует поле, создает новых муравьев и проверяет условия завершения игры.
-        """
+        '''
         clear_screen()
         self.field.draw_rows()
         self.field.spawn_ants()
 
         if self.field.game_over:
-            print("Все муравьи съедены или сбежали. Игра окончена!")
+            print('Все муравьи съедены или сбежали. Игра окончена!')
 
     def run(self):
-        """
+        '''
         Запускает игру и обрабатывает события до завершения игры.
-        """
+        '''
         self.field.draw_rows()
 
         while not self.field.game_over:
